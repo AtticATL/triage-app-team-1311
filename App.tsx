5695c89f509df108880cc9366f9b5369f35ef34b
William Goodall <wgoodall01@gmail.com>
2022-01-30 02:35:32 -0500

    Add NativeBase, pin Node version to 16

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { NativeBaseProvider } from "native-base";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +14,10 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
+      <NativeBaseProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
-      </SafeAreaProvider>
+      </NativeBaseProvider>
     );
   }
 }
