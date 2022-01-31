257dff4957522f787d1b5691bd8e72102499897f
William Goodall <wgoodall01@gmail.com>
2022-01-30 19:49:02 -0500

    Automatically generate JSON-schema for profile data

---
+++ scripts/generateProfileSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import zodToJsonSchema from "zod-to-json-schema";
+import { Profile } from "../lib/profile";
+
+// Generate the schema
+const jsonSchema = zodToJsonSchema(Profile, "profile");
+
+console.log(JSON.stringify(jsonSchema, null, 4));
