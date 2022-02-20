160cafc4db60e8d834e7d2adde39a2cee15b247a
Ibrahim Abotaleb <ibrahimabotaleb@Ibrahims-MacBook-Pro.local>
2022-02-20 15:23:38 -0500

    Add URL field validation

--- screens/RecieveProfileScreen.tsx
+++ screens/RecieveProfileScreen.tsx
@@ -2,21 +2,38 @@ import * as React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavSubProps as RootNavSubProps } from "../App";
 import { Text, Heading, VStack, Input, Button } from "native-base";
-import { TextField } from "../components/Form";
+import { TextField, useField } from "../components/Form";
 import TileButton from "../components/TileButton";
+import { z } from "zod";
+import { setupURLPolyfill } from 'react-native-url-polyfill';
+
+setupURLPolyfill();
+
+/**
+ * The URL validator for the patient profile
+ */
+export const Url = z
+    .string()
+    .url({ message: "Please insert a valid URL"});
+export type Url = z.infer<typeof Url>
 
 export default function RecieveProfileScreen({
   navigation,
 }: RootNavSubProps<"RecieveProfile">) {
-  const url = "";
+
+  const url = useField(Url);
 
   return (
     <VStack mx={4} mt={4} space={8} safeAreaBottom safeAreaX pb={16}>
-      <Text>INSRET INSTRUCTIONS HERE</Text>
-      <Input placeholder="URL"></Input>
+      <Text>INSERT INSTRUCTIONS HERE</Text>
+      <TextField field={url}
+                label="Patient profile URL"
+                help="The provided link to the wanted patient profile"
+                size="2xl"
+      />
       <Button
         onPress={() => {
-          // console.log('Click')
+          console.log('Click')
           navigation.navigate("ViewProfile");
         }}
       >
