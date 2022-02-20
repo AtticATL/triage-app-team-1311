d2fdf63744b2741d3d635255fa1a0e900e60ccc1
Ibrahim Abotaleb <iabotaleb3@gatech.edu>
2022-02-20 16:35:15 -0500

    Add validation requirement for button

--- screens/RecieveProfileScreen.tsx
+++ screens/RecieveProfileScreen.tsx
@@ -1,43 +1,48 @@
 import * as React from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavSubProps as RootNavSubProps } from "../App";
-import { Text, Heading, VStack, Input, Button } from "native-base";
+import { Text, Heading, VStack, Input, Button, Icon } from "native-base";
 import { TextField, useField } from "../components/Form";
 import TileButton from "../components/TileButton";
 import { z } from "zod";
-import { setupURLPolyfill } from 'react-native-url-polyfill';
+import { setupURLPolyfill } from "react-native-url-polyfill";
+import { Feather } from "@expo/vector-icons";
 
 setupURLPolyfill();
 
 /**
  * The URL validator for the patient profile
  */
-export const Url = z
-    .string()
-    .url({ message: "Please insert a valid URL"});
-export type Url = z.infer<typeof Url>
+export const Url = z.string().url({ message: "Please insert a valid URL" });
+export type Url = z.infer<typeof Url>;
 
 export default function RecieveProfileScreen({
   navigation,
 }: RootNavSubProps<"RecieveProfile">) {
-
   const url = useField(Url);
+  const validURL = url.validator.safeParse(url.value).success;
 
   return (
-    <VStack mx={4} mt={4} space={8} safeAreaBottom safeAreaX pb={16}>
-      <Text>INSERT INSTRUCTIONS HERE</Text>
-      <TextField field={url}
-                label="Patient profile URL"
-                help="The provided link to the wanted patient profile"
-                size="2xl"
+    <VStack mx={4} mt={8} space={6}>
+      <TextField
+        field={url}
+        label="Patient profile URL"
+        help="The provided link to the wanted patient profile"
+        size="2xl"
       />
       <Button
         onPress={() => {
-          console.log('Click')
-          navigation.navigate("ViewProfile");
+          if (validURL) {
+            navigation.navigate("ViewProfile");
+          }
         }}
+        isDisabled={!validURL}
+        py={8}
+        size="lg"
+        colorScheme="success"
+        rightIcon={<Icon as={Feather} name="arrow-right" size="sm" />}
       >
-        <Text>Continue</Text>
+        Continue
       </Button>
     </VStack>
   );
