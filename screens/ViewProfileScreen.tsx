68b05742dd57c2950c85b89787883030c3b8375c
Flynt Wallace <flyntwallace77@gmail.com>
2022-02-20 19:04:21 -0500

    Prettified ViewProfileScreen

--- screens/ViewProfileScreen.tsx
+++ screens/ViewProfileScreen.tsx
@@ -1,17 +1,125 @@
 import * as React from "react";
-import { Stack, Text } from "native-base";
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  ScrollView,
+  Stack,
+  Text,
+  useColorModeValue,
+  VStack,
+} from "native-base";
 import { NavSubProps as RootNavSubProps } from "../App";
 import { Profile } from "../lib/profile";
+import { CHECKLIST, QUESTIONS } from "../lib/triageQuestions";
+import BlobMedia from "../components/BlobMedia";
+import { Entry } from "../components/Form";
+import { Feather } from "@expo/vector-icons";
 
 export default function ViewProfileScreen({
   route,
   navigation,
 }: RootNavSubProps<"ViewProfile">) {
   const profile: Profile = route.params.profile;
+  const bgProps = useColorModeValue("#DEDEDE", "#121212");
 
   return (
-    <Stack>
-      <Text>{JSON.stringify(profile, null, 2)}</Text>
-    </Stack>
+    <ScrollView>
+      <VStack p={8} space={8}>
+        <Heading size="xl">Patient Profile</Heading>
+
+        <VStack space={4}>
+          <Text fontSize="xl" bold>
+            {profile.identity.name}
+          </Text>
+
+          <HStack alignItems="center">
+            <Text fontSize="md" color="#6F6F6F">
+              AGE{" "}
+            </Text>
+            <Text fontSize="lg">
+              {new Date().getFullYear() - profile.identity.birthYear}
+            </Text>
+            <Text fontSize="md" color="#6F6F6F">
+              {" "}
+              SEX
+            </Text>
+            <Text fontSize="lg"> {profile.identity.sex}</Text>
+          </HStack>
+
+          <Flex direction="row" mb="2.5" mt="2.5"></Flex>
+
+          <VStack bg={bgProps} p={4} rounded={9}>
+            <Text fontSize="2xl">{"    "}History</Text>
+          </VStack>
+
+          <VStack p={5} space={5}>
+            <VStack>
+              <Text fontSize="lg" bold>
+                Medical History:
+              </Text>
+              <Text fontSize="lg">{profile.patientHistory.pastHistory}</Text>
+            </VStack>
+
+            <VStack>
+              <Text fontSize="lg" bold>
+                History of Current Infection:
+              </Text>
+              <Text fontSize="lg">
+                {profile.patientHistory.currentInfectionHistory}
+              </Text>
+            </VStack>
+          </VStack>
+
+          <VStack bg={bgProps} p={4} rounded={9}>
+            <Text fontSize="2xl">{"    "}Triage Checklist</Text>
+          </VStack>
+
+          <VStack p={4} space={4}>
+            {CHECKLIST.map((id) => (
+              <Checkbox
+                colorScheme="dark"
+                value={id}
+                isChecked={profile.triageChecklist[id]}
+              >
+                <Text fontSize="md">{"  " + QUESTIONS[id].text}</Text>
+              </Checkbox>
+            ))}
+          </VStack>
+
+          <VStack bg={bgProps} p={4} rounded={9}>
+            <Text fontSize="2xl">{"    "}Imagery</Text>
+          </VStack>
+
+          {profile.attachments.map((attachment) => (
+            <Entry
+              space={2}
+              px={2}
+              py={2}
+              key={attachment.blob.sha256}
+              rounded={20}
+            >
+              <BlobMedia hash={attachment.blob.sha256} />
+              <HStack justifyContent="center" alignItems="center"></HStack>
+            </Entry>
+          ))}
+
+          <VStack bg={bgProps} p={4} rounded={9}>
+            <Text fontSize="2xl">{"    "}Other Notes</Text>
+          </VStack>
+
+          <VStack p={5} space={5}>
+            <Text fontSize="lg">{profile.patientHistory.otherNotes}</Text>
+          </VStack>
+        </VStack>
+      </VStack>
+    </ScrollView>
+
+    // <Stack>
+    //   <Text>{JSON.stringify(profile, null, 2)}</Text>
+    // </Stack>
   );
 }
